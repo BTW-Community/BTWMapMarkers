5becd695ad7d60f09c898993da710c9e6d2170a7
Bo Ramsey <bramsey@trshealthcare.com>
2022-03-04 21:56:03 -0600

    added positioning and some cleanup

--- src/client/java/net/minecraft/src/SMMBlockMapMarker.java
+++ src/client/java/net/minecraft/src/SMMBlockMapMarker.java
@@ -8,8 +8,16 @@ import static net.minecraft.src.SledgesMapMarkersAddon.mapMarkerItem;
 public class SMMBlockMapMarker extends BlockContainer {
 
     double m_dBlockHeight = 1D;
-    double m_dBlockWidth = (1D / 16D);
+    double m_dBlockWidth = (2D / 16D);
     double m_dBlockHalfWidth = (m_dBlockWidth / 2D);
+
+    //AxisAlignedBB shaftBox = AxisAlignedBB.getAABBPool().getAABB(
+    AxisAlignedBB shaftBox = new AxisAlignedBB(
+            0.5D - m_dBlockHalfWidth, 0D, 0.5D - m_dBlockHalfWidth,
+            0.5D + m_dBlockHalfWidth, m_dBlockHeight, 0.5D + m_dBlockHalfWidth);
+    AxisAlignedBB flagBox = new AxisAlignedBB(
+            shaftBox.minX, shaftBox.maxY - 0.25D, shaftBox.minZ - (6D / 16D),
+            shaftBox.maxX, shaftBox.maxY, shaftBox.minZ);
 
     public SMMBlockMapMarker(int blockId) {
         super(blockId, Material.wood);
@@ -21,7 +29,7 @@ public class SMMBlockMapMarker extends BlockContainer {
     }
 
     @Override
-    public AxisAlignedBB getCollisionBoundingBoxFromPool( World world, int x, int y, int z )
+    public AxisAlignedBB getCollisionBoundingBoxFromPool(World world, int x, int y, int z)
     {
         return null;
     }
@@ -40,30 +48,26 @@ public class SMMBlockMapMarker extends BlockContainer {
 
     @Override
     public boolean canPlaceBlockOnSide(World world, int x, int y, int z, int side) {
-        FCUtilsBlockPos targetPos = new FCUtilsBlockPos( x, y, z, Block.GetOppositeFacing( side ) );
-        if ( FCUtilsWorld.DoesBlockHaveCenterHardpointToFacing( world, targetPos.i, targetPos.j, targetPos.k, side ) )
-        {
-            int iTargetID = world.getBlockId( targetPos.i, targetPos.j, targetPos.k );
+        FCUtilsBlockPos targetPos = new FCUtilsBlockPos(x, y, z, Block.GetOppositeFacing( side));
+        if (FCUtilsWorld.DoesBlockHaveCenterHardpointToFacing(world, targetPos.i, targetPos.j, targetPos.k, side)) {
+            int iTargetID = world.getBlockId(targetPos.i, targetPos.j, targetPos.k);
             return CanStickInBlockType(iTargetID);
         }
         return false;
     }
 
-    public boolean CanStickInBlockType( int blockId )
-    {
+    public boolean CanStickInBlockType(int blockId) {
         Block block = Block.blocksList[blockId];
         return block != null;
     }
 
     @Override
-    public boolean canPlaceBlockAt( World world, int x, int y, int z )
-    {
-        for ( int iFacing = 0; iFacing < 6; iFacing++ )
-        {
-            FCUtilsBlockPos targetPos = new FCUtilsBlockPos( x, y, z, iFacing );
+    public boolean canPlaceBlockAt(World world, int x, int y, int z) {
+        for (int facing = 0; facing < 6; facing++) {
+            FCUtilsBlockPos targetPos = new FCUtilsBlockPos(x, y, z, facing);
 
-            if ( FCUtilsWorld.DoesBlockHaveCenterHardpointToFacing( world, targetPos.i, targetPos.j, targetPos.k,
-                    Block.GetOppositeFacing( iFacing ) ) )
+            if (FCUtilsWorld.DoesBlockHaveCenterHardpointToFacing(world, targetPos.i, targetPos.j, targetPos.k,
+                    Block.GetOppositeFacing(facing)))
             {
                 return true;
             }
@@ -72,9 +76,10 @@ public class SMMBlockMapMarker extends BlockContainer {
         return canPlaceOn(world, x, y - 1, z);
     }
 
-    protected boolean canPlaceOn( World world, int x, int y, int z)
-    {
-        return FCUtilsWorld.DoesBlockHaveSmallCenterHardpointToFacing( world, x, y, z, 1, true );
+    protected boolean canPlaceOn( World world, int x, int y, int z) {
+        int facing = world.getBlockMetadata(x, y, z);
+        return FCUtilsWorld.DoesBlockHaveSmallCenterHardpointToFacing(world, x, y, z,
+                Block.GetOppositeFacing(facing), true);
     }
 
     @Override
@@ -96,100 +101,58 @@ public class SMMBlockMapMarker extends BlockContainer {
     }
 
     @Override
-    public int idDropped(int metaData, Random random, int fortuneModifier )
+    public int idDropped(int metaData, Random random, int fortuneModifier)
     {
         return mapMarkerItem.itemID;
     }
 
     @Override
-    public void onNeighborBlockChange( World world, int x, int y, int z, int neighborBlockId )
-    {
-        if (!FCUtilsWorld.DoesBlockHaveCenterHardpointToFacing(world, x, y - 1, z, 1))
-        {
-            this.dropBlockAsItem(world, x, y, z, world.getBlockMetadata(x, y, z), 0);
-            world.setBlockToAir(x, y, z);
+    public void onNeighborBlockChange(World world, int x, int y, int z, int neighborBlockId) {
+        int facing = GetFacing(world, x, y, z);
+
+        if (!canPlaceBlockOnSide(world, x, y, z, Block.GetOppositeFacing(facing))) {
+            // pop the block off if it no longer has a valid anchor point
+            dropBlockAsItem(world, x, y, z, world.getBlockMetadata(x, y, z), 0);
+            world.setBlockWithNotify(x, y, z, 0);
         }
     }
 
     @Override
-    public int onBlockPlaced(World world, int x, int y, int z, int side, float clickX, float clickY, float clickZ, int metaData)
+    public boolean HasSmallCenterHardPointToFacing(IBlockAccess blockAccess, int x, int y, int z, int facing,
+                                                   boolean ignoreTransparency)
     {
-        return metaData;
-//        int alignment;
-//
-//        float xOffsetFromCenter = Math.abs( clickX - 0.5F );
-//        float yOffsetFromCenter = Math.abs( clickY - 0.5F );
-//        float zOffsetFromCenter = Math.abs( clickZ - 0.5F );
-//
-//        switch ( side )
-//        {
-//            case 0:
-//                if ( xOffsetFromCenter > zOffsetFromCenter )
-//                {
-//                    alignment = clickX > 0.5F ? 9 : 11;
-//                }
-//                else
-//                {
-//                    alignment = clickZ > 0.5F ? 10 : 8;
-//                }
-//                break;
-//            case 1:
-//                if ( xOffsetFromCenter > zOffsetFromCenter )
-//                {
-//                    alignment = clickX > 0.5F ? 1 : 3;
-//                }
-//                else
-//                {
-//                    alignment = clickZ > 0.5F ? 2 : 0;
-//                }
-//                break;
-//            case 2:
-//                if ( xOffsetFromCenter > yOffsetFromCenter )
-//                {
-//                    alignment = clickX > 0.5F ? 6 : 7;
-//                }
-//                else
-//                {
-//                    alignment = clickY > 0.5F ? 10 : 2;
-//                }
-//                break;
-//            case 3:
-//                if ( xOffsetFromCenter > yOffsetFromCenter )
-//                {
-//                    alignment = clickX > 0.5F ? 5 : 4;
-//                }
-//                else
-//                {
-//                    alignment = clickY > 0.5F ? 8 : 0;
-//                }
-//                break;
-//            case 4:
-//                if ( zOffsetFromCenter > yOffsetFromCenter )
-//                {
-//                    alignment = clickZ > 0.5F ? 6 : 5;
-//                }
-//                else
-//                {
-//                    alignment = clickY > 0.5F ? 9 : 1;
-//                }
-//                break;
-//            default: // 5
-//                if ( zOffsetFromCenter > yOffsetFromCenter )
-//                {
-//                    alignment = clickZ > 0.5F ? 7 : 4;
-//                }
-//                else
-//                {
-//                    alignment = clickY > 0.5F ? 11 : 3;
-//                }
-//                break;
-//        }
-//
-//        return alignment;
+        // only has upwards facing hard point for torches
+        return facing == 1 && GetFacing( blockAccess, x, y, z ) == 0;
+    }
+
+    public boolean CanBeCrushedByFallingEntity(World world, int x, int y, int z, EntityFallingSand entity) {
+        return true;
+    }
+
+    public void OnCrushedByFallingEntity(World world, int x, int y, int z, EntityFallingSand entity) {
+        if ( !world.isRemote ) {
+            dropBlockAsItem(world, x, y, z, world.getBlockMetadata(x, y, z), 0);
+        }
     }
 
     @Override
-    public boolean onBlockActivated( World world, int x, int y, int z, EntityPlayer player, int facing, float xClick, float yClick, float zClick )
+    public void OnNeighborDisrupted(World world, int x, int y, int z, int toFacing) {
+        if (toFacing == GetFacing(world, x, y, z)) {
+            dropBlockAsItem(world, x, y, z, world.getBlockMetadata(x, y, z), 0);
+            world.setBlockWithNotify(x, y, z, 0);
+        }
+    }
+
+    @Override
+    public int onBlockPlaced(World world, int x, int y, int z, int side, float clickX, float clickY, float clickZ,
+                             int metaData)
+    {
+        return side;
+    }
+
+    @Override
+    public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int facing, float xClick,
+                                    float yClick, float zClick)
     {
         SMMTileEntityMapMarker tile = (SMMTileEntityMapMarker) world.getBlockTileEntity(x, y, z);
         if (tile == null) {
@@ -202,13 +165,12 @@ public class SMMBlockMapMarker extends BlockContainer {
     }
 
     @Override
-    public boolean CanGroundCoverRestOnBlock( World world, int x, int y, int z )
-    {
-        return world.doesBlockHaveSolidTopSurface( x, y - 1, z );
+    public boolean CanGroundCoverRestOnBlock(World world, int x, int y, int z) {
+        return world.doesBlockHaveSolidTopSurface(x, y - 1, z);
     }
 
     @Override
-    public float GroundCoverRestingOnVisualOffset( IBlockAccess blockAccess, int x, int y, int z )
+    public float GroundCoverRestingOnVisualOffset(IBlockAccess blockAccess, int x, int y, int z)
     {
         return -1F;
     }
@@ -220,63 +182,66 @@ public class SMMBlockMapMarker extends BlockContainer {
     private Icon[] iconFlags;
 
     @Override
-    public void registerIcons( IconRegister register )
-    {
+    public void registerIcons(IconRegister register) {
         iconStake = register.registerIcon("fcBlockLogChewedOak_side");
         blockIcon = iconStake;
 
         iconFlags = new Icon[16];
 
-        for (int i = 0; i < iconFlags.length; ++i)
-        {
+        for (int i = 0; i < iconFlags.length; ++i) {
             iconFlags[i] = register.registerIcon("cloth_" + i);
         }
     }
 
     @Override
-    public boolean shouldSideBeRendered( IBlockAccess blockAccess, int neighborX, int neighborY, int neighborZ, int side )
+    public boolean shouldSideBeRendered(IBlockAccess blockAccess, int neighborX, int neighborY, int neighborZ,
+                                        int side)
     {
         return true;
     }
 
     @Override
-    public int idPicked( World world, int x, int y, int z )
-    {
-        return idDropped( world.getBlockMetadata( x, y, z ), world.rand, 0 );
+    public int idPicked(World world, int x, int y, int z) {
+        return idDropped( world.getBlockMetadata(x, y, z ), world.rand, 0);
     }
 
     @Override
-    protected AxisAlignedBB GetFixedBlockBoundsFromPool() {
-        return AxisAlignedBB.getAABBPool().getAABB(
-                0.5D - m_dBlockHalfWidth, 0D, 0.5D - m_dBlockHalfWidth,
-                0.5D + m_dBlockHalfWidth, m_dBlockHeight, 0.5D + m_dBlockHalfWidth)
-                .expand(0D, 0D, (3D / 16D))
-                .offset(0D, 0D, -(3D / 16D));
+    public AxisAlignedBB GetBlockBoundsFromPoolBasedOnState(IBlockAccess blockAccess, int x, int y, int z) {
+        int metaData = blockAccess.getBlockMetadata(x, y, z);
+
+        AxisAlignedBB box = shaftBox.MakeTemporaryCopy();
+        box.ExpandToInclude(flagBox);
+        box = getBoxPosition(box, metaData);
+
+        return box;
+    }
+
+    private AxisAlignedBB getBoxPosition(AxisAlignedBB box, int position) {
+        box = box.MakeTemporaryCopy();
+        box.RotateAroundJToFacing(position);
+        box.TiltToFacingAlongJ(position);
+        return box;
     }
 
     @Override
-    public boolean RenderBlock( RenderBlocks renderer, int x, int y, int z )
-    {
-        AxisAlignedBB shaft = AxisAlignedBB.getAABBPool().getAABB(
-                0.5D - m_dBlockHalfWidth, 0D, 0.5D - m_dBlockHalfWidth,
-                0.5D + m_dBlockHalfWidth, m_dBlockHeight, 0.5D + m_dBlockHalfWidth);
-        AxisAlignedBB flag = new AxisAlignedBB(shaft.minX, shaft.maxY - 0.25D, shaft.minZ - (6D / 16D), shaft.maxX, shaft.maxY, shaft.minZ);
+    public boolean RenderBlock(RenderBlocks renderer, int x, int y, int z) {
+        int metaData = renderer.blockAccess.getBlockMetadata(x, y, z);
 
-        //shaft
-        renderer.setRenderBounds(shaft);
-        renderer.renderStandardBlock( this, x, y, z);
+        AxisAlignedBB shaft = getBoxPosition(shaftBox, metaData);
+        AxisAlignedBB flag = getBoxPosition(flagBox, metaData);
 
         SMMTileEntityMapMarker tileEntity = (SMMTileEntityMapMarker) renderer.blockAccess.getBlockTileEntity(x, y, z);
-        int iconFileIndex;
+        int iconFileIndex = 0;
         if (tileEntity != null) {
             iconFileIndex = tileEntity.GetIconFileIndex();
             //System.out.println("RenderBlock: " + tileEntity.GetMarkerId() + " iconIndex = " + tileEntity.GetIconIndex() + ", IconFileIndex = " + iconFileIndex);
         }
-        else {
-            iconFileIndex = 0;
-            //System.out.println("RenderBlock: missing tile entity");
-        }
-        //flag
+
+        //shaftBox
+        renderer.setRenderBounds(shaft);
+        renderer.renderStandardBlock( this, x, y, z);
+
+        //flagBox
         renderer.unlockBlockBounds();
         renderer.setRenderBounds(flag);
         renderer.setOverrideBlockTexture(iconFlags[iconFileIndex]);
