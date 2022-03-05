0886332a0ccb98494554f31aa76776b50d43cd8a
Bo Ramsey <bramsey@trshealthcare.com>
2022-03-04 22:05:11 -0600

    Incremented version

--- src/client/java/net/minecraft/src/SledgesMapMarkersAddon.java
+++ src/client/java/net/minecraft/src/SledgesMapMarkersAddon.java
@@ -18,7 +18,7 @@ public class SledgesMapMarkersAddon extends FCAddOn {
     public static Map WorldMapMarkers = new LinkedHashMap<String, SMMMapMarkerData>();
 
     private SledgesMapMarkersAddon() {
-        super("Sledge Map Markers", "0.2.4", "SMM");
+        super("Sledge Map Markers", "0.3.0", "SMM");
     }
 
     @Override
