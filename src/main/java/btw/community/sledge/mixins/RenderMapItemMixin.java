381a448ce360b935369d6beeea8421b15b53e85e
psychosledge <psychosledge@gmail.com>
2024-01-30 17:19:09 -0600

    Added overload with group to avoid crashes due to local variable differences between dev and release

--- src/main/java/btw/community/sledge/mixins/RenderMapItemMixin.java
+++ src/main/java/btw/community/sledge/mixins/RenderMapItemMixin.java
@@ -2,10 +2,7 @@ package btw.community.sledge.mixins;
 
 import net.minecraft.src.*;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.Slice;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
@@ -22,10 +19,23 @@ public abstract class RenderMapItemMixin {
 
     }
 
+    @Group(name = "addVertexes", min = 1, max = 1)
     @Inject(method = "renderMap",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/src/Tessellator;draw()I", ordinal = 1, shift = At.Shift.BEFORE),
-            locals = LocalCapture.CAPTURE_FAILHARD)
-    private void addVertexes(EntityPlayer par1EntityPlayer, RenderEngine par2RenderEngine, MapData par3MapData, CallbackInfo ci, byte var15, byte var16, Tessellator var17, float var18, int var19, Iterator var20, MapCoord var21, float var22, float var23, float var24, float var25) {
+            locals = LocalCapture.CAPTURE_FAILSOFT)
+    private void addVertexesRelease(EntityPlayer par1EntityPlayer, RenderEngine par2RenderEngine, MapData par3MapData, CallbackInfo ci, int var15, int var16, Tessellator var17, float var18, int var19, Iterator var20, MapCoord var21) {
+        addVertexesHandler(var17, var19, var21);
+    }
+
+    @Group(name = "addVertexes", min = 1, max = 1)
+    @Inject(method = "renderMap",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/src/Tessellator;draw()I", ordinal = 1, shift = At.Shift.BEFORE),
+            locals = LocalCapture.CAPTURE_FAILSOFT)
+    private void addVertexesDev(EntityPlayer par1EntityPlayer, RenderEngine par2RenderEngine, MapData par3MapData, CallbackInfo ci, byte var15, byte var16, Tessellator var17, float var18, int var19, Iterator var20, MapCoord var21) {
+        addVertexesHandler(var17, var19, var21);
+    }
+
+    private void addVertexesHandler(Tessellator var17, int var19, MapCoord var21) {
         int modified = (var21.iconSize & 255);
         float modifiedVar22 = (float)(modified % 4 + 0) / 4.0F;
         float modifiedVar23 = (float)(modified / 4 + 0) / 4.0F;
